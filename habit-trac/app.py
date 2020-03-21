1a6e4e6e0648bec3d33cd33468383f3f97afc89d
Arjun11 <arjun.johri7@gmail.com>
2020-03-21 14:13:46 -0400

    app.js +mongo db+ app.py creation

--- habit-trac/app.py
+++ habit-trac/app.py
@@ -1,21 +1,25 @@
 # import necessary libraries
 import os
+import datetime as dt
 from flask import (
     Flask,
     render_template,
     jsonify,
     request,
     redirect)
-from flask_pymongo import PyMongo 
+import pymongo
 
 # Flask Setup
 app = Flask(__name__, static_url_path='/static')
 
 # Use PyMongo to establish Mongo connection
 conn='mongodb://heroku_0zrl06lj:egmnlm1vtil4nk48igf4gkqipd@ds141043.mlab.com:41043/heroku_0zrl06lj'
-client = PyMongo.MongoClient(conn)
-db= client.habit_db
-collection = db.
+client = pymongo.MongoClient(conn)
+db = client["habit_db"]
+habit = db["habit"]
+habit_dashboard = db["habit_dashboard"]
+user_data = db["user_data"]
+print("running")
 
 #import psycopg2
 # create route that renders index.html template
@@ -24,33 +28,31 @@ def home():
     return render_template("index.html")
 # create route that renders country.html template
 @app.route("/dashboard")
-def country():
+def dashboard():
     return render_template("/dashboard.html")
 # create route that renders insights.html template
 @app.route("/recommendations")
-def insights():
+def recommendations():
     return render_template("/recommendations.html")
 # create route that renders data.html template
 
-@app.route("/api/habitmodify")
-def habitmodify():
-    session = Session(engine)
-    results = session.query(Emissions.indicator,Emissions.country,Emissions.year).all()
-    return jsonify(emission_data)
-# for the graph on Country page
-@app.route("/api/updatedash")
-def updatedashboard(grabhabitmod):
-    session = Session(engine)
-    results = session.query(Emissions.indicator, Emissions.unit,Emissions.country,Emissions.year,Emissions.value,Emissions.variable).\
-        filter(grabindicator == Emissions.indicator).\
-        filter(grabcountry == Emissions.country).all()
-    return jsonify(dash_data)
+@app.route("/api/habitadd/<userid>/<newhabit>/<priority>")
+def habitadd(userid,newhabit,priority):
+    datetoday=dt.date.today()
 
-# for the World graph on Home page 
-@app.route("/api/updatereco")
-def worldgraph(grabdash):
-    session = Session(engine)
-    results = session.query(Emissions.indicator, Emissions.unit,Emissions.country,Emissions.year,Emissions.value).\
-        filter(grabindicator == Emissions.indicator).\
-        filter("World" == Emissions.country).all()
-    return jsonify(emission_data)
+    insertdict = {
+        "user_id":userid,
+        "habit_name": newhabit,
+        "habit_act_date": datetoday,
+        "habit_priority": priority
+    }
+
+    # Update the Mongo database using update and upsert=True
+    habit.insert_one(insertdict)
+
+    result=habit.find()
+    print(result)
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run()
--- habit-trac/static/js/app.js
+++ habit-trac/static/js/app.js
@@ -6,49 +6,69 @@ mydoc.on("click",addorremove)
 
 // grabbing the habit and adding another row for the next option
 function addorremove(event){
-  var clickedbutton = d3.event.target  // find which button is clicked
+  var clickedbutton = d3.event.target.id  // find which button is clicked
   console.log(clickedbutton)
-}
+
+  if (clickedbutton == "add"){
+
+    var newhabit  = d3.selectAll("#habitname").property("value")
+    var userid  = 1
+    var priority  = d3.selectAll("#habitpriority").property("value")
+
+    window.globalhabitcounter++ //counting the number of habits
+    var urlhabit = "/api/habitadd/" + userid +"/"+ newhabit +"/"+ priority
+    console.log(urlhabit)  
+    d3.json(urlhabit).then(function(response){
+      console.log(response)     // from the API it will get the json response as habit name, habit priority.
+      response.forEach(([user_id,habit_name,habit_act_date,habit_priority]) => {
+        rowadd=d3.select("tbody").append("tr").classed(`habit${window.globalhabitcounter}`)
+        rowadd.append("td").text(window.globalhabitcounter);
+        rowadd.append("td").text(habit_name);
+        rowadd.append("td").text(habit_priority);
+        rowadd.append("td").append("button").id(`delete${window.globalhabitcounter}`).text("Remove");
+      });
+    }).catch(function(error) {
+      console.log(error);
+    });
+    
+}}
+
 // d3.select("#delete0").on("click",addhabit)
 // function addhabit(event){
 //   // find which button is clicked
 //   console.log((d3.event.target.id).slice(7,8)  )
 // }
 
-if (clickedbutton == "#add*") {
-  d3.select("#add").on("click",addhabit)
-  function addhabit(){
-    var habit  = d3.selectAll(`#habit${window.globalhabitcounter}`).property("value")
-    var urlhabit = "/api/habitadd/" + habit
-    console.log(urlhabit) 
+// if (clickedbutton == "#add*") {
+//   d3.select("#add").on("click",addhabit)
+//   function addhabit(){
+//     var habit  = d3.selectAll(`#habit${window.globalhabitcounter}`).property("value")
+//     var urlhabit = "/api/habitadd/" + habit
+//     console.log(urlhabit) 
 
-    window.globalhabitcounter++ //counting the number of habits
-    var row = d3.select("#habitcont").append("div").classed(`.row rownum${window.globalhabitcounter}`)
-    row.append("div").classed(".col-md-10")
-      .append("h3")
-      .append("input").classed(".form-control").id(`#habit${window.globalhabitcounter}`).type("text")
-    var row2=row.append("div").classed(".col-md-2")
-    row2.append("button").id(`#delete${window.globalhabitcounter}`).text("Remove")
-    row2.append("button").id(`#add${window.globalhabitcounter}`).text("Add")
-  }
-}
-else if ( clickedbutton =="#delete*") {
-  num=clickedbutton.slice(7,8)
-  d3.select(`#delete${num}`).on("click",removehabit)  // grabbing the habit and remove that row
-  function removehabit(){
-    var habit  = d3.selectAll(`#habit${num}`).property("value")
-    var urlhabit = "/api/habitremove/" + habit
-    console.log(urlhabit) 
+//     window.globalhabitcounter++ //counting the number of habits
+//     var row = d3.select("#habitcont").append("div").classed(`row rownum${window.globalhabitcounter}`)
+//     row.append("div").classed("col-md-10")
+//       .append("h3")
+//       .append("input").classed("form-control").id(`habit${window.globalhabitcounter}`).type("text")
+//     var row2=row.append("div").classed("col-md-2")
+//     row2.append("button").id(`#delete${window.globalhabitcounter}`).text("Remove")
+//     row2.append("button").id(`#add${window.globalhabitcounter}`).text("Add")
+//   }
+// }
+// else if ( clickedbutton =="#delete*") {
+//   num=clickedbutton.slice(7,8)
+//   d3.select(`#delete${num}`).on("click",removehabit)  // grabbing the habit and remove that row
+//   function removehabit(){
+//     var habit  = d3.selectAll(`#habit${num}`).property("value")
+//     var urlhabit = "/api/habitremove/" + habit
+//     console.log(urlhabit) 
 
-    d3.select("#habitcont").drop("div").classed(`.row rownum${num}`)
+//     d3.select("#habitcont").drop("div").classed(`.row rownum${num}`)
     
-    d3.json(urlhabit).then(function(response){
-      dashboard(response)
-    })
+//     d3.json(urlhabit).then(function(response){
+//       dashboard(response)
+//     })
 
-}
-}
-
-
-
-
+// }
+// }
